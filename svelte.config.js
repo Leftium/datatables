2b8eeeed00a54e477d0b9940872b27b710363a9a
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-02-06 23:38:44 +0900

    Switch to adapter-auto.

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,26 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: ['.svelte', ...mdsvexConfig.extensions],
-    preprocess: [
-        mdsvex(mdsvexConfig),
-        preprocess(), 
-    ],
+    preprocess: [mdsvex(mdsvexConfig), preprocess()],
     kit: {
         adapter: adapter({ out: 'build' }),
         paths: {
             base: '/datatables'
         }
     },
-	vitePlugin: {
+    vitePlugin: {
         inspector: {
             toggleKeyCombo: 'meta-shift',
             holdMode: true,
             showToggleButton: 'always',
-            toggleButtonPos: 'bottom-right',
+            toggleButtonPos: 'bottom-right'
         }
-	}
+    }
 }
 
 export default config
